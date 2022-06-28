439c80f4853ce31019d37b06594263a68327a25a
Kris Nóva <kris@nivenly.com>
2022-06-28 14:56:20 -0400

    Automatic commit from github.com/kris-nova/bin/git-save
    Signed-off-by: Kris Nóva <kris@nivenly.com>

--- auraefs/dir_mkdir.go
+++ auraefs/dir_mkdir.go
@@ -30,5 +30,8 @@ var _ fs.NodeMkdirer = &Dir{}
 func (n *Dir) Mkdir(ctx context.Context, name string, mode uint32, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
 	logrus.Debugf("%s --[d]--> Mkdir(%s)", n.path, name)
 	_, dir := n.NewSubDir(ctx, path.Join(n.path, name))
+	if dir == nil {
+		return nil, 0
+	}
 	return &dir.Inode, 0
 }
