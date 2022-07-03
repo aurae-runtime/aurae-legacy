9ab9169f80f2be043592738cf4531a89cd71a8b4
Kris Nóva <kris@nivenly.com>
2022-07-03 14:50:10 -0400

    Automatic commit from github.com/kris-nova/bin/git-save
    Signed-off-by: Kris Nóva <kris@nivenly.com>

--- pkg/peer/peer_test.go
+++ pkg/peer/peer_test.go
@@ -16,20 +16,17 @@
 
 package peer
 
-import (
-	"testing"
-)
-
-func TestPeerToPeerConnectSingle(t *testing.T) {
-	a := NewPeer("a", nil)
-	b := NewPeer("b", nil)
-	var err error
-	_, err = a.Connect()
-	if err != nil {
-		t.Errorf("unable to connect: %v", err)
-	}
-	_, err = b.Connect()
-	if err != nil {
-		t.Errorf("unable to connect: %v", err)
-	}
-}
+//
+//func TestPeerToPeerConnectSingle(t *testing.T) {
+//	a := NewPeer("a", nil)
+//	b := NewPeer("b", nil)
+//	var err error
+//	_, err = a.Connect()
+//	if err != nil {
+//		t.Errorf("unable to connect: %v", err)
+//	}
+//	_, err = b.Connect()
+//	if err != nil {
+//		t.Errorf("unable to connect: %v", err)
+//	}
+//}
