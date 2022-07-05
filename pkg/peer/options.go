c687f251bcd18ddd4aaf7bb77439b3bd6b9c17cb
Kris Nóva <kris@nivenly.com>
2022-07-05 13:52:11 -0400

    Automatic commit from github.com/kris-nova/bin/git-save
    Signed-off-by: Kris Nóva <kris@nivenly.com>

--- pkg/peer/options.go
+++ pkg/peer/options.go
@@ -18,10 +18,12 @@ package peer
 
 import (
 	"crypto/rand"
+	"fmt"
 	p2p "github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/crypto"
-	"github.com/multiformats/go-multiaddr"
 )
+
+const Port int = 1719
 
 // DefaultOptions are the default p2p options for Aurae
 func DefaultOptions() []p2p.Option {
@@ -35,17 +37,17 @@ func DefaultOptions() []p2p.Option {
 		// EnableAutoRelay for performance reasons in the circuit.
 		p2p.EnableAutoRelay(),
 
-		p2p.ListenAddrs(
-			// Listen on ipv4, choose an available port
-			//multiaddr.StringCast("/ip4/0.0.0.0/tcp/0")),
+		//p2p.ListenAddrs(
+		// Listen on ipv4, choose an available port
+		//multiaddr.StringCast("/ip4/0.0.0.0/tcp/0")),
 
-			// Listen on the default p2p-circuit
-			//multiaddr.StringCast("/p2p-circuit"),
+		// Listen on the default p2p-circuit
+		//multiaddr.StringCast("/p2p-circuit"),
 
-			// Listen on IPv6, choose an available port
-			multiaddr.StringCast("/ip6/::/tcp/0")),
+		// Listen on IPv6, choose an available port
+		//multiaddr.StringCast("/ip6/::/tcp/0")),
 
-		//p2p.ListenAddrStrings(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", listenPort)),
+		p2p.ListenAddrStrings(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", Port)),
 
 		p2p.DefaultTransports,
 		p2p.DefaultMuxers,
