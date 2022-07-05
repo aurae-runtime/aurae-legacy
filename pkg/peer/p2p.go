c68245523b3c8fac0895eca1fbe48b8ad0e96b43
Kris Nóva <kris@nivenly.com>
2022-07-05 10:28:33 -0400

    Automatic commit from github.com/kris-nova/bin/git-save
    Signed-off-by: Kris Nóva <kris@nivenly.com>

--- pkg/peer/p2p.go
+++ pkg/peer/p2p.go
@@ -20,7 +20,6 @@ import (
 	"crypto/rand"
 	p2p "github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/crypto"
-	"github.com/multiformats/go-multiaddr"
 )
 
 // DefaultOptions are the default p2p options for Aurae
@@ -33,15 +32,15 @@ func DefaultOptions() []p2p.Option {
 		// EnableAutoRelay for performance reasons in the circuit.
 		p2p.EnableAutoRelay(),
 
-		p2p.ListenAddrs(
-			// Listen on ipv4, choose an available port
-			//multiaddr.StringCast("/ip4/0.0.0.0/tcp/0")),
+		//p2p.ListenAddrs(
+		// Listen on ipv4, choose an available port
+		//multiaddr.StringCast("/ip4/0.0.0.0/tcp/0")),
 
-			// Listen on the default p2p-circuit
-			//multiaddr.StringCast("/p2p-circuit"),
+		// Listen on the default p2p-circuit
+		//multiaddr.StringCast("/p2p-circuit"),
 
-			// Listen on IPv6, choose an available port
-			multiaddr.StringCast("/ip6/::/tcp/0")),
+		// Listen on IPv6, choose an available port
+		//multiaddr.StringCast("/ip6/::/tcp/0")),
 	}
 
 }
