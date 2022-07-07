92d3dec0ece5490b11fd0bb40c4aa7aad4ad76e9
Kris Nóva <kris@nivenly.com>
2022-07-07 07:30:29 -0400

    Automatic commit from github.com/kris-nova/bin/git-save
    Signed-off-by: Kris Nóva <kris@nivenly.com>

--- pkg/peer/peer_test.go
+++ pkg/peer/peer_test.go
@@ -48,3 +48,41 @@ func TestPeerToPeerHandshake(t *testing.T) {
 	}
 
 }
+
+func TestPeerToPeerHandshakeMultiplex(t *testing.T) {
+
+	ctx := context.Background()
+	var err error
+
+	p1 := NewPeer(name.New("p1"))
+	err = p1.Establish(ctx, 0)
+	if err != nil {
+		t.Errorf("unable to establish p1: %v", err)
+	}
+	p2 := NewPeer(name.New("p1"))
+	err = p2.Establish(ctx, 1)
+	if err != nil {
+		t.Errorf("unable to establish p2: %v", err)
+	}
+
+	// p2 -> p1
+	err = p1.HandshakeServe()
+	if err != nil {
+		t.Errorf("unable to start handshake serve on p1: %v", err)
+	}
+	err = p2.Handshake(p1.Host().ID())
+	if err != nil {
+		t.Errorf("unable to handshake from p2 -> p1: %v", err)
+	}
+
+	// p1 -> p2
+	err = p2.HandshakeServe()
+	if err != nil {
+		t.Errorf("unable to start handshake serve on p2: %v", err)
+	}
+	err = p1.Handshake(p2.Host().ID())
+	if err != nil {
+		t.Errorf("unable to handshake from p1 -> p2: %v", err)
+	}
+
+}
