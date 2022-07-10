7584cfc2a4122d413793697e382242f6049ed467
Kris Nóva <kris@nivenly.com>
2022-07-10 18:25:27 -0400

    Automatic commit from github.com/kris-nova/bin/git-save
    Signed-off-by: Kris Nóva <kris@nivenly.com>

--- pkg/peer/options.go
+++ pkg/peer/options.go
@@ -17,9 +17,9 @@
 package peer
 
 import (
-	"fmt"
 	p2p "github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/crypto"
+	"github.com/multiformats/go-multiaddr"
 )
 
 // DefaultOptions are the default p2p options for Aurae
@@ -39,24 +39,25 @@ func DefaultOptions(key crypto.PrivKey, offset int) []p2p.Option {
 		// This is also suggested for the NAT hole punching.
 		p2p.EnableAutoRelay(),
 
-		//p2p.ListenAddrs(
-		// Listen on ipv4, choose an available port
-		//multiaddr.StringCast("/ip4/0.0.0.0/tcp/0"),
-
-		// Listen on the default p2p-circuit
-		//multiaddr.StringCast("/p2p-circuit"),
-
-		// Listen on IPv6, choose an available port
-		//multiaddr.StringCast("/ip6/::/tcp/0")),
-
-		p2p.ListenAddrStrings(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", DefaultListenPort+offset)),
-
 		p2p.DefaultTransports,
 		p2p.DefaultMuxers,
 		p2p.DefaultSecurity,
 		p2p.EnableNATService(),   // Convenience service for peers
 		p2p.NATPortMap(),         // Try to open a port in the network firewall using UPnP
 		p2p.EnableHolePunching(), // STUN hole punching
+
+		p2p.ListenAddrs(
+			// Listen on ipv4, choose an available port
+			multiaddr.StringCast("/ip4/0.0.0.0/tcp/0"),
+
+			// Listen on the default p2p-circuit
+			multiaddr.StringCast("/p2p-circuit"),
+
+			// Listen on IPv6, choose an available port
+			multiaddr.StringCast("/ip6/::/tcp/0")),
+
+		//p2p.ListenAddrStrings(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", DefaultListenPort+offset)),
+
 	}
 
 }
