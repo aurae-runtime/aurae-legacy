6ec9bd1608b5fc4e5c2d7287c55c2417800b7235
Kris Nóva <kris@nivenly.com>
2022-08-06 13:49:48 -0400

    Automatic commit from github.com/kris-nova/bin
    Signed-off-by: Kris Nóva <kris@nivenly.com>

--- pkg/runtime/run.go
+++ pkg/runtime/run.go
@@ -20,6 +20,8 @@ import (
 	"github.com/firecracker-microvm/firecracker-go-sdk"
 	"github.com/weaveworks/ignite/pkg/apis/ignite"
 	"github.com/weaveworks/ignite/pkg/container"
+	"github.com/weaveworks/libgitops/pkg/runtime"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func RunContainer(name string) error {
@@ -29,5 +31,37 @@ func RunContainer(name string) error {
 	//client := firecracker.NewClient("/run/firecracker.socket", logrus.NewEntry(logrus.New()), true)
 	vm := &ignite.VM{}
 	vm.SetName(name)
-	return container.ExecuteFirecracker(vm, firecracker.NetworkInterfaces{})
+	vm.SetImage(&ignite.Image{
+		TypeMeta: runtime.TypeMeta{
+			TypeMeta: v1.TypeMeta{
+				Kind:       "",
+				APIVersion: "",
+			},
+		},
+		ObjectMeta: runtime.ObjectMeta{
+			Name:        name,
+			Labels:      nil,
+			Annotations: nil,
+		},
+		Spec: ignite.ImageSpec{},
+	})
+	netinterfaces := []firecracker.NetworkInterface{
+		{
+			StaticConfiguration: &firecracker.StaticNetworkConfiguration{
+				MacAddress:  "",
+				HostDevName: "aurae",
+				//IPConfiguration: &firecracker.IPConfiguration{
+				//	IPAddr: net.IPNet{
+				//		IP:   net.IP{},
+				//		Mask: nil,
+				//	},
+				//	Gateway:     nil,
+				//	Nameservers: nil,
+				//	IfName:      "",
+				//},
+			},
+			AllowMMDS: false,
+		},
+	}
+	return container.ExecuteFirecracker(vm, netinterfaces)
 }
