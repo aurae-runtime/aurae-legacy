1bfa9cf39a1d6b52fc136797cbbbe3e74f841f64
Kris Nóva <kris@nivenly.com>
2022-08-21 15:55:38 -0400

    Automatic commit from github.com/kris-nova/bin
    Signed-off-by: Kris Nóva <kris@nivenly.com>

--- pkg/printer/printer.go
+++ pkg/printer/printer.go
@@ -17,7 +17,6 @@
 package printer
 
 import (
-	"fmt"
 	"reflect"
 )
 
@@ -38,52 +37,60 @@ func AnyToConsole(title string, v interface{}) *Console {
 		r = reflect.Indirect(r)
 	}
 	rType := r.Type()
-
-	// HasLists
-	hasLists := false
+	kv := NewKeyValueTable("")
 	for i := 0; i < r.NumField(); i++ {
 		f := r.Field(i)
-		if f.Kind() == reflect.Map {
-			hasLists = true
-			break
-		}
-		if f.Kind() == reflect.Slice {
-			hasLists = true
+		if f.CanInterface() {
+			kv.AddKeyValue(rType.Field(i).Name, f.Interface())
 		}
 	}
+	c.AddPrinter(kv)
 
-	if hasLists {
-		// Table Mode
-		t := NewTable("")
-		for i := 0; i < r.NumField(); i++ {
-			f := r.Field(i)
-			pf := t.NewField(rType.Field(i).Name)
-			if f.Kind() == reflect.Map {
-				if len(f.MapKeys()) == 0 {
-					pf.AddValue("---------")
-				} else {
-					for _, k := range f.MapKeys() {
-						v := f.MapIndex(k)
-						pf.AddValue(fmt.Sprintf("%s:%s", k, v))
-					}
-				}
-			} else {
-				pf.AddValue(f.Interface())
-			}
-			t.AddField(pf)
-
-		}
-		c.AddPrinter(t)
-	} else {
-		// Key Value Mode
-		kv := NewKeyValueTable("")
-		for i := 0; i < r.NumField(); i++ {
-			f := r.Field(i)
-			if f.CanInterface() {
-				kv.AddKeyValue(fmt.Sprintf("%v %v", rType.Field(i).Name, f.Type()), f.Interface())
-			}
-		}
-		c.AddPrinter(kv)
-	}
+	//// HasLists
+	//hasLists := false
+	//for i := 0; i < r.NumField(); i++ {
+	//	f := r.Field(i)
+	//	if f.Kind() == reflect.Map {
+	//		hasLists = true
+	//		break
+	//	}
+	//	if f.Kind() == reflect.Slice {
+	//		hasLists = true
+	//	}
+	//}
+	//
+	//if hasLists {
+	//	// Table Mode
+	//	t := NewTable("")
+	//	for i := 0; i < r.NumField(); i++ {
+	//		f := r.Field(i)
+	//		pf := t.NewField(rType.Field(i).Name)
+	//		if f.Kind() == reflect.Map {
+	//			if len(f.MapKeys()) == 0 {
+	//				pf.AddValue("---------")
+	//			} else {
+	//				for _, k := range f.MapKeys() {
+	//					v := f.MapIndex(k)
+	//					pf.AddValue(fmt.Sprintf("%s:%s", k, v))
+	//				}
+	//			}
+	//		} else {
+	//			pf.AddValue(f.Interface())
+	//		}
+	//		t.AddField(pf)
+	//
+	//	}
+	//	c.AddPrinter(t)
+	//} else {
+	//	// Key Value Mode
+	//	kv := NewKeyValueTable("")
+	//	for i := 0; i < r.NumField(); i++ {
+	//		f := r.Field(i)
+	//		if f.CanInterface() {
+	//			kv.AddKeyValue(fmt.Sprintf("%v %v", rType.Field(i).Name, f.Type()), f.Interface())
+	//		}
+	//	}
+	//	c.AddPrinter(kv)
+	//}
 	return c
 }
